e0f45f05af15ab3ac2e639f954654c4c81a327eb
Chafloks <chafloquesiu@gmail.com>
2020-07-23 22:26:00 -0400

    api key

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ba4d0111a250e1c91cd1fc61c397482c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCUZNICcaDtXVa6LzfiH6pqCew-yW4SySI"
