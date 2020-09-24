cde9c278d22c08012e59d4d424272644c522de94
Chafloks <chafloquesiu@gmail.com>
2020-09-23 21:49:27 -0400

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "ba4d0111a250e1c91cd1fc61c397482c"
+weather_api_key = "Key"
 
 # Google API Key
-g_key = "AIzaSyCcpsGROHuhUJnQ0sF-lM7a9RRDjGHcHaE"
+g_key = "Key"
